b082806a5cf37248e9f9d5e4fd3ce63d6e41b869
lateefa <aldoseri369@gmail.com>
2025-03-25 14:47:51 +0300

    styling

--- PawTrack/src/components/NavBar/NavBar.jsx
+++ PawTrack/src/components/NavBar/NavBar.jsx
@@ -6,38 +6,97 @@ const NavBar = ({ handleSignout }) => {
   const user = useContext(AuthedUserContext);
 
   return (
-    <>
-      {user ? (
-        <nav>
-          <ul>
-            <li>Welcome, {user.username}</li> 
-            <li><Link to="/">Home page</Link> </li>
-            <li><Link to='/posts'>Pets Posts</Link></li>
-            <li><Link to='/messages'>DM</Link></li>
-            <li>
-              <Link to="/posts/new">Add Post</Link> 
+    <nav
+      className="navbar navbar-expand-lg px-4"
+      style={{ backgroundColor: '#f5e8ff', boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)' }}
+    >
+      <div className="container-fluid d-flex align-items-center justify-content-between">
+        {/* شعار أو اسم الموقع */}
+        <Link
+          className="navbar-brand fw-bold"
+          to="/"
+          style={{ color: '#6a0dad', fontSize: '1.7rem' }}
+        >
+          PawTrack
+        </Link>
+
+        <div className="d-flex align-items-center">
+          <ul className="navbar-nav d-flex gap-4">
+            <li className="nav-item">
+              <Link className="nav-link" style={linkStyle} to="/">
+                Home
+              </Link>
             </li>
-            <li>
-              <Link to="" onClick={handleSignout}>
-                Sign Out
-              </Link> 
+            <li className="nav-item">
+              <Link className="nav-link" style={linkStyle} to="/posts">
+                Pets Posts
+              </Link>
             </li>
+            {user && (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" style={linkStyle} to="/messages">
+                    DM
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" style={linkStyle} to="/posts/new">
+                    Add Post
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    style={linkStyle}
+                    to=""
+                    onClick={handleSignout}
+                  >
+                    Sign Out
+                  </Link>
+                </li>
+              </>
+            )}
+            {!user && (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" style={linkStyle} to="/signin">
+                    Sign In
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" style={linkStyle} to="/signup">
+                    Sign Up
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
-        </nav>
-      ) : (
-        <nav>
-          <ul>
-            <li>
-              <Link to="/signin">Sign In</Link>
-            </li>
-            <li>
-              <Link to="/signup">Sign Up</Link>
-            </li>
-          </ul>
-        </nav>
-      )}
-    </>
+
+          {user && (
+            <span
+              className="ms-4"
+              style={{
+                color: '#6a0dad',
+                fontWeight: 'bold',
+                fontSize: '1.5rem',
+              }}
+            >
+              Welcome, {user.username}
+            </span>
+          )}
+        </div>
+      </div>
+    </nav>
   );
 };
 
+// CSS-in-JS لتنسيق الروابط
+const linkStyle = {
+  fontSize: '1.1rem',
+  fontWeight: '500',
+  color: '#4b0082',
+  transition: 'color 0.3s ease',
+};
+
 export default NavBar;
+
