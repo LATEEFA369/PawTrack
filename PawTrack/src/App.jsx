a3167f27ab8358fdaf37f408f609483ea6a88340
lateefa <aldoseri369@gmail.com>
2025-03-26 10:28:11 +0300

    styling

--- PawTrack/src/App.jsx
+++ PawTrack/src/App.jsx
@@ -2,10 +2,9 @@ import { useState, createContext, useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Landing from './components/Landing/Landing';
-import Home  from './components/Home/Home';
+import Home from './components/Home/Home';
 import SignupForm from './components/SignupForm/SignupForm';
 import SigninForm from './components/SigninForm/SigninForm';
-
 
 import * as authService from '../src/services/authService'; // import the authservice
 
@@ -22,47 +21,48 @@ import Terms from './components/Terms/Term';
 import FAQ from './components/FAQ/FAQ';
 
 const App = () => {
-  const [user, setUser] = useState(authService.getUser());
-  const [posts, setPosts] = useState([]);
   const navigate = useNavigate();
+  const [user, setUser] = useState(authService.getUser()); // using the method from authservice
 
   const handleSignout = () => {
     authService.signout();
     setUser(null);
   };
-//===================================================================================
-//anything related to posts:
 
- 
- 
-useEffect(() => {
-  const fetchAllPosts = async () => {
-    if (!user) {
-      console.log("No user is currently signed in.");
-      return; 
-    }
-    try {
+  //===================================================================================
+  //anything related to posts:
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchAllPosts = async () => {
       const postsData = await postService.index();
       setPosts(postsData);
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-    }
+    };
+    if (user) fetchAllPosts();
+  }, [user]); // we added this so when another user sign in, use effect again because another user
+
+  //delete post
+  const handleDeletePost = async (postId) => {
+    // Call upon the service function:
+    const deletedPost = await postService.deletePost(postId);
+    // Filter state using deletedpost._id:
+    setPosts(posts.filter((post) => post._id !== deletedPost._id));
+    // Redirect the user:
+    navigate('/posts');
   };
-  fetchAllPosts();
-}, [user]); // we added this so when another user sign in, use effect again because another user 
 
+  //update post
+  const handleUpdatePost = async (postId, postFormData) => {
+    const updatedPost = await postService.update(postId, postFormData);
+    setPosts(posts.map((post) => (postId === post._id ? updatedPost : post)));
+    navigate(`/posts/${postId}`);
+  };
 
-
-//===================================================================================
   const handleAddPost = async (formData) => {
-    try {
-      const newPost = await postService.create(formData);
-      const newPostList = [newPost, ...posts];
-      setPosts(newPostList);
-      navigate('/posts');
-    } catch (error) {
-      console.error('Error creating post:', error);
-    }
+    const newPost = await postService.create(formData);
+    const newPostList = [newPost, ...posts];
+    setPosts(newPostList);
+    navigate('/posts');
   };
 
   return (
@@ -74,28 +74,27 @@ useEffect(() => {
             <>
               <Route path="/" element={<Home user={user} />} />
               <Route path="/posts" element={<PostList posts={posts} />} />
-              <Route path="/posts/:postId" element={<PostDetails />} />
-              <Route path="/posts" element={<PostList posts={posts} />} /> 
+              <Route path="/posts/:postId" element={<PostDetails handleDeletePost={handleDeletePost} />} />
+              <Route
+                path="/posts/:postId/edit"
+                element={<PostForm handleUpdatePost={handleUpdatePost} />}
+              />
               <Route path="/messages" element={<DM />} />
               <Route path="/posts/new" element={<PostForm handleAddPost={handleAddPost} />} />
-              <Route path="/" element={<Home />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/terms" element={<Terms />} />
             </>
           ) : (
-            //for visitor
             <Route path="/" element={<Landing />} />
           )}
           <Route path="/signup" element={<SignupForm setUser={setUser} />} />
           <Route path="/signin" element={<SigninForm setUser={setUser} />} />
         </Routes>
-        <Footer user={user}/>
+        <Footer user={user} />
       </AuthedUserContext.Provider>
     </>
   );
 };
 
 export default App;
-
-
 
